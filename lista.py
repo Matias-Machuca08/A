c18fe93e4a09fe3317877a59e8a5440bb6a4df56
Kuhchau <134294093+Matias-Machuca08@users.noreply.github.com>
2023-05-23 09:44:03 -0400

    Update lista.py
    actualizacion

--- lista.py
+++ lista.py
@@ -1,12 +1,25 @@
 lista = [1,2,3,4] #indice inicial del cero
+lista2 = ["hola","chao", True, 1.5, 100]
 
 print (lista)
 
 print("valor del indice 1 (parte del cero): ", lista [1])
 print("valor del indice 3 (parte del cero): ", lista [3])
 
-lista2 = ["hola","chao", True, 1.5, 100]
+
 print("valor del indice 1: ", lista2 [1])
 print("valor del indice 4: ", lista2 [4])
 print("valor del indice 4: ", lista2 [-1])
 print("valor del indice 4: ", lista2 [-2])
+
+
+print("cantidad de elementos: ", len(lista))
+
+lista.append(100) #back box
+print(lista)
+print("cantidad de elementos: ", len(lista))
+
+lista.reverse()
+print(lista)
+
+lista.insert
